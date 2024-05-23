2ba4c3ec35bdb223878d82671eef36773f81db6f
start.vaadin.com <no-reply@vaadin.com>
2024-05-23 16:01:53 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/mainview/MainViewView.java
@@ -0,0 +1,59 @@
+package com.example.application.views.mainview;
+
+import com.example.application.data.SamplePerson;
+import com.example.application.services.SamplePersonService;
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+
+@PageTitle("Main View")
+@Route(value = "", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+@Uses(Icon.class)
+public class MainViewView extends Composite<VerticalLayout> {
+
+    public MainViewView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        Grid basicGrid = new Grid(SamplePerson.class);
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        layoutColumn2.setJustifyContentMode(JustifyContentMode.START);
+        layoutColumn2.setAlignItems(Alignment.START);
+        layoutColumn2.setAlignSelf(FlexComponent.Alignment.START, basicGrid);
+        basicGrid.setWidth("100%");
+        basicGrid.getStyle().set("flex-grow", "0");
+        setGridSampleData(basicGrid);
+        getContent().add(layoutRow);
+        layoutRow.add(layoutColumn2);
+        layoutColumn2.add(basicGrid);
+    }
+
+    private void setGridSampleData(Grid grid) {
+        grid.setItems(query -> samplePersonService.list(
+                PageRequest.of(query.getPage(), query.getPageSize(), VaadinSpringDataHelpers.toSpringDataSort(query)))
+                .stream());
+    }
+
+    @Autowired()
+    private SamplePersonService samplePersonService;
+}
